3e7c186c22c022411302a95f9f205c76b58c7747
leotfr <leotafforeau@gmail.com>
2025-02-26 22:04:06 +0100

    third

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - checking digits characters
+ * Description: Write a function that multiplies two integers.
+ * @a: first digit
+ * @b: second digit
+ * Return: The result
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
