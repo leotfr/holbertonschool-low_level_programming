3a4a02ab0d85c17417d6039eacf8794714e9a42e
leotfr <leotafforeau@gmail.com>
2025-02-26 22:08:49 +0100

    quattre

---
+++ more_functions_nested_loops/3-print_number.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers - Print digits 0 to 9
+ * Description: Write a function that prints digits
+ */
+
+void print_numbers(void)
+{
+int	i;
+for (i = 0; i <= 9; i++)
+_putchar ('0' + i);
+_putchar ('\n');
+}
