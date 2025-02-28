a5202eee5d1d6402fc3fccbd89e72932df6c8d8b
leotfr <leotafforeau@gmail.com>
2025-02-28 14:21:38 +0100

    5step

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - Write a function that reverses a string.
+ * @s: The reversed string
+ */
+void rev_string(char *s)
+{
+	int length = 0;
+	int i;
+	char temp;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	for (i = 0; i < length / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[length - 1 - i];
+		s[length - 1 - i] = temp;
+	}
+}
