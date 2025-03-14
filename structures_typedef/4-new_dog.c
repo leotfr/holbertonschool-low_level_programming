7bd9b45b3e1fc4fe0b13504562eac30718bfcffe
leotfr <leotafforeau@gmail.com>
2025-03-14 14:55:59 +0100

    4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,40 +1,48 @@
+#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "dog.h"
 
 /**
- * new_dog - Creates a new dog
- * @name: Name of the dog
- * @age: Age of the dog
- * @owner: Owner of the dog
+ * new_dog - Initialize a variable of type struct dog
+ * @name: pointer to the dog's name
+ * @owner: pointer to the dog's owner
+ * @age: age of dog
+ * Return: pointer to new_dog or NULL if it fails
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
-	char *name_copy, *owner_copy;
+	unsigned int name_length = 0, owner_length = 0, i;
 
+	if (name == NULL || owner == NULL)
+		return (NULL);
+	while (name[name_length])
+		name_length++;
+	while (owner[owner_length])
+		owner_length++;
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
-
-	name_copy = strdup(name);
-	if (name_copy == NULL)
+	new_dog->name = malloc(name_length + 1);
+	if (new_dog->name == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
-
-	owner_copy = strdup(owner);
-	if (owner_copy == NULL)
+	for (i = 0; i < name_length; i++)
+		new_dog->name[i] = name[i];
+	new_dog->name[name_length] = '\0';
+	new_dog->owner = malloc(owner_length + 1);
+	if (new_dog->owner == NULL)
 	{
-		free(name_copy);
+		free(new_dog->name);
 		free(new_dog);
 		return (NULL);
 	}
 
-	new_dog->name = name_copy;
+	for (i = 0; i < owner_length; i++)
+		new_dog->owner[i] = owner[i];
+	new_dog->owner[owner_length] = '\0';
 	new_dog->age = age;
-	new_dog->owner = owner_copy;
-
 	return (new_dog);
 }
